9bb3549a77fa4c88119f077ff45ed9987d81c945
shaohai <isshaohai@163.com>
2021-01-24 13:00:09 +0800

    feat: webpack5 前端微服务实现

--- 25-预留作业/ms-image/src/components/ms-image/ms-image.js
+++ 25-预留作业/ms-image/src/components/ms-image/ms-image.js
@@ -1,15 +1,19 @@
 import Girls from './girls.jpg';
-import "./ms-image.scss";
+import './ms-image.scss';
 class MsImage {
-  render() {
-    const img = document.createElement('img');
-    img.src = Girls;
-    img.alt = 'Girls';
-    img.classList.add('ms-image');
-
-    const bodyDomElement = document.querySelector('body');
-    bodyDomElement.appendChild(img);
-  }
+    render(bodyDomElement) {
+        const img = document.createElement('img');
+        img.src = Girls;
+        img.alt = 'Girls';
+        img.classList.add('ms-image');
+        if (!bodyDomElement) {
+            bodyDomElement = document.querySelector('body');
+        } else {
+            bodyDomElement.innerHTML = '';
+        }
+        console.log('bodyDomElement', bodyDomElement);
+        bodyDomElement.appendChild(img);
+    }
 }
 
 export default MsImage;
--- 25-预留作业/ms-image/webpack.production.config.js
+++ 25-预留作业/ms-image/webpack.production.config.js
@@ -1,75 +1,81 @@
-const path = require("path");
-const TerserPlugin = require("terser-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { ModuleFederationPlugin } = require("webpack").container;
-
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {ModuleFederationPlugin} = require('webpack').container;
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "./dist"),
-    publicPath: "/static/"
-  },
-  mode: "production",
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-      minSize: 3000
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpg)$/,
-        use: [
-          "file-loader"
-        ]
-      },
-
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader, 'css-loader', "sass-loader"
-        ]
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/env"],
-            plugins: ["@babel/plugin-proposal-class-properties"]
-          }
+    entry: './src/index.js',
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/static/'
+    },
+    mode: 'production',
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+            minSize: 3000
         }
-      }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg)$/,
+                use: ['file-loader']
+            },
+
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env'],
+                        plugins: ['@babel/plugin-proposal-class-properties']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new TerserPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: [
+                '**/*',
+                path.join(process.cwd(), 'build/**/*')
+            ]
+        }),
+        new HtmlWebpackPlugin({
+            title: 'ms-image',
+            filename: 'ms-image.html',
+            template: 'index.html',
+            meta: {
+                description: 'ms-image'
+            },
+            minify: false
+        }),
+        new ModuleFederationPlugin({
+            name: 'MsButtonApp',
+            remotes: {
+                MsButtonApp: 'MsButtonApp@http://localhost:9001/remoteEntry.js'
+            }
+        }),
+        // 一定要填写：output.publicPath，否则出不来效果
+        new ModuleFederationPlugin({
+            name: 'MsImageApp',
+            filename: 'remoteEntry.js', // http://localhost:9002/remoteEntry.js
+            exposes: {
+                './MsImage': './src/components/ms-image/ms-image.js'
+            }
+        })
     ]
-  },
-  plugins: [
-    new TerserPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css"
-    }),
-    new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: ['**/*', path.join(process.cwd(), 'build/**/*')],
-    }),
-    new HtmlWebpackPlugin({
-      title: "ms-image",
-      filename: "ms-image.html",
-      template: "index.html",
-      meta: {
-        description: "ms-image"
-      },
-      minify: false
-    }),
-    new ModuleFederationPlugin({
-      name: "MsImageApp",
-      remotes: {
-        MsButtonApp: "MsButtonApp@http://localhost:9001/remoteEntry.js"
-      }
-    })
-  ]
-}
+};
